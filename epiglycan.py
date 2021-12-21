4809e3b2322d8474384c0d9b337d6bb082daf6ac
rocfd <56597768+rocfd@users.noreply.github.com>
2021-12-21 10:05:54 +0100

    paper update

--- epiglycan.py
+++ epiglycan.py
@@ -1,33 +1,20 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  1 18:16:13 2020
-Description: Classification of glycosylated epitopes given epitope position and glycosylated residues' position in the parental protein sequence.
-Input: epitope sequence, parental protein sequence & glycosilated positions.
-Output: epitope glycosilation: YES or NO.
-@author: rocfarriolduran
-"""
-
 # IMPORTS
 import sys
 import csv
 import os.path
 from pathlib import Path
+import pandas as pd
 
 # HELP
 h = '''
     To right usage of this script:
-        $ python3 busca3030.py
+        $ python3 epiglycan.py
     The files in use have to be provided by stating "location/file_name.csv"
     to the input questions that appear in the console.
     <file_name> should be a .csv separated by ";"
-    OPTIONAL you can provide coma separated glycans
+    Additionally you have to provide coma separated glycans
         For example: 0,16,32,64
-        If you provide the golas....????????????
-    The script returns a <file_name>_out.xlsx as output.
-    You can also do the following (if using Mac/Linux OS):
-        $ chmod +x busca3030.py
-        $ ./busca3030.py <location/file_name.csv> <comaSeparateGoals>
+    The script returns a <file_name>_out.csv as output.
     You need python 3 installed in your computer!!!
     '''
 
@@ -45,12 +32,6 @@ def fileExist(file):
     else:
         return False
 
-
-# VARIABLES
-data_file=""
-inputGlycans=""
-glycans=""
-
 num_args=len(sys.argv)
 
 
@@ -59,13 +40,18 @@ if num_args>=2:
 if num_args==3:
     inputGlycans = sys.argv[2]
 
+# VARIABLES
+data_file=""
+inputGlycans=""
+glycans=""
+
 # FILE NOT EXIST
 while not fileExist(data_file):
     data_file = input(''' Provide location and name of the file dataset.
         For example: data/file_test.csv
             (-h for help)
         Here: ''')
-print("Input file: " + data_file)
+print("Input file:" + data_file)
 
 #data_file= 'test_protein_parser.csv'
 
@@ -75,70 +61,44 @@ lenextension=len(extension)
 nameOutFile=data_file[:-lenextension]+"_Out"+extension
 
 # GLYCAN INPUT
-if num_args<3:
-    inputGlycans = input(''' OPTIONAL Provide coma separated glycans.
-        (zero is first position)
-        For example: 0,16,32,64
+while not fileExist(inputGlycans):
+    inputGlycans = input(''' Provide location and name of the file dataset.
+        For example: data/file_test.csv
+            (-h for help)
         Here: ''')
+print("Input file:" + inputGlycans)
 
-# GLYCAN CHECK   
-#glycans=[0,16,32,64]
-if len(inputGlycans)>0:
-    glycans=inputGlycans.split(",")
+## READ DATA
+data = pd.read_csv(data_file, sep = ";")
+data
 
-if len(glycans)!=0:
-    print("glycans: " + inputGlycans)
-else:
-    print("No glycans provided")
+## READ GLYCANS (single column file with "glyc_pos" as column title and positions in separate rows)
+glycans_df = pd.read_csv(inputGlycans, sep = ",")
+glycans_df
 
-# GLYCAN DETECTION SCRIPT
-def isOnGoal(start,llpep):
-    isOnGoal=-1
-    if len(glycans) > 0:
-        for goal in glycans:
-            if start<=int(goal) and int(goal)<=start+llpep-1:
-                if isOnGoal==-1:
-                    isOnGoal=goal
-                else:
-                    isOnGoal = isOnGoal + "," + goal
-    return isOnGoal
+## DF TO LIST
+glycans_list = glycans_df["glyc_pos"].values.tolist()
+glycans_list
+type(glycans_list)
 
-# OUTPUT FILE
-out_file=open(nameOutFile, "w")
-if len(glycans) > 0:
-    out_file.writelines("Id;Sequence;Peptide;Position;Pre;lenPre;Post;lenPost;onGoal\n")
-else:
-    out_file.writelines("Id;Sequence;Peptide;Position;Pre;lenPre;Post;lenPost\n")
+## LOOP TO DETERMINE GLYCOSILATED EPITOPES
+z = []
+for index, row in data.iterrows():
+    for glyc in glycans_list:
+        y = "Non-glycosilated"
+        if glyc >= row["Start"] and int(glyc) <= row["Start"] + row["Length"] - 1:
+            y = "Glycosilated"
+            z.append(y)
+            break
+    if y == "Non-glycosilated":
+        z.append(y)
+print(z)
+len(z)
 
-f=open(data_file, "r", encoding = 'utf-8-sig')
-inputFile = csv.reader(f, delimiter=';')
-y=0
-for reg in inputFile :
-    if y!=0:
-        id=reg[0]
-        seq=reg[1]
-        pep=reg[2]
-        pre=""
-        post=""
-        llpep=len(pep)
-        start=seq.find(pep)
-        if start!=-1:
-            if start<30 :
-                if start==0:
-                    pre=""
-                else:
-                    pre=seq[:start-1]
-            else:
-                pre=seq[start-31:start-1]
-            post=seq[start+llpep+1:start+llpep+31]
-            if len(glycans)>0:
-                onGoal=isOnGoal(start,llpep)
+## APPEND GLYCOSILATION TO DF
+data['Glycosilation'] = z
+data
 
-        if len(glycans) > 0:
-            out_file.writelines(id+";"+seq+";"+pep+";"+str(start)+";"+pre+";"+str(len(pre))+";"+post+";"+str(len(post))+";"+str(onGoal)+ "\n")
-        else:
-            out_file.writelines(id+";"+seq+";"+pep+";"+str(start)+";"+pre+";"+str(len(pre))+";"+post+";"+str(len(post))+"\n")
-    y=y+1
-
-out_file.close()
-print("Output file: "+nameOutFile)
+## EXPORT DATA
+data.to_csv(path_or_buf= nameOutFile,
+         index = True, index_label = "Rank")
