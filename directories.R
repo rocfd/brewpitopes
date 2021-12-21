26856e4d988b0ec114f0869d04430269a1a80bfe
rocfd <56597768+rocfd@users.noreply.github.com>
2021-12-21 12:29:08 +0100

    Create directories.R

---
+++ directories.R
@@ -0,0 +1,24 @@
+## MAKE DIRECTORIES FOR BREWPITOPES
+
+## INITIAL DATA - FASTA
+dir.create("Z_fasta")
+
+## DIRECTORIES
+dir.create("A_linear_predictions")
+dir.create("A_linear_predictions/bebipred")
+dir.create("A_linear_predictions/abcpred")
+dir.create("B_structural_predictions")
+dir.create("B_structural_predictions/discotope")
+dir.create("B_structural_predictions/pdb")
+dir.create("B_structural_predictions/alphafold")
+dir.create("C_epixtractor")
+dir.create("D_epimerger")
+dir.create("E_epitoplogy")
+dir.create("E_epitoplogy/CCTOP")
+dir.create("F_epiglycan")
+dir.create("F_epiglycan/netnglyc")
+dir.create("F_epiglycan/netoglyc")
+dir.create("G_episurf")
+dir.create("H_epifilter")
+dir.create("I_final_candidates")
+dir.create("J_plots")
