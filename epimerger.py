195d94fef74544220a2aadf1a62307f95488b02c
rocfd <56597768+rocfd@users.noreply.github.com>
2022-09-28 10:33:26 +0200

    epimerger automated

--- epimerger.py
+++ epimerger.py
@@ -1,89 +1,85 @@
 ### EPIMERGER
 
-# GOAL: MERGE LINEAR EPITOPES FROM BEBIPRED AND ABCPRED AND DISCOTOPE
+# GOAL: MERGE LINEAR EPITOPES FROM bepiPRED AND ABCPRED AND DISCOTOPE
 
 ## LIBRARIES
-library(tidyr)
-library(tibble)
-library(dplyr)
-library(data.table)
+library(tidyr, quietly = T, warn.conflicts = F)
+library(tibble, quietly = T, warn.conflicts = F)
+library(dplyr, quietly = T, warn.conflicts = F)
+library(data.table, quietly = T, warn.conflicts = F)
+library(argparser, quietly = T, warn.conflicts = F)
+
+### SCRIPT ARGUMENTS
+# Create a parser
+p <- arg_parser("EPIMERGER")
+
+# Add command line arguments
+p <- add_argument(p, "--abcpred", help= "Path to abcpred extracted epitopes (.csv format)", type="character", default = "./brewpitopes/C_epixtractor/abcpred_extracted.csv")
+p <- add_argument(p, "--bepipred", help= "Path to bepipred extracted epitopes (.csv format)", type="character", default = "./brewpitopes/C_epixtractor/bepipred_extracted.csv")
+p <- add_argument(p, "--discotope", help= "Path to discotope extracted epitopes (.csv format)", type="character", default = "./brewpitopes/C_epixtractor/discotope_extracted.csv")
+p <- add_argument(p, "--sample", help = "Sample name to label output files", type = "character", default = "merged")
+p <- add_argument(p, "--outdir", help = "Path to output files", type = "character", default = "./brewpitopes/D_epimerger")
+# p <- add_argument(p, "--save_rdata_dir", help = "Path to save rData image", type = "character", default = "./brewpitopes/D_epimerger")
+
+# Parse the command line arguments
+argv <- parse_args(p)
 
 ## IMPORT RESULTS
 ## IMPORT ABCPRED RESULTS EXTRACTED USING EPIXTRACTOR
-abc <- read.csv("path/to/abcpred/results_allmers.csv",sep = ";")
+abc <- read.csv(file = argv$abcpred,sep = ";")
 ## IMPORT BEPIPRED RESULTS EXTRACTED USING EPIXTRACTOR
-bebi <- read.csv(file = "path/to/bepipred/results_Out.csv", sep = ";")
-colnames(abc)
-colnames(bebi)
+bepi <- read.csv(file = argv$bepipred, sep = ";")
 
-## ADD EXTRA COLUMNS TO BEBI
-bebi$Sequence <- as.character(bebi$Sequence)
-bebi$Tool <- "Bepipred 2.0"
+## ADD EXTRA COLUMNS TO bepi
+bepi$Sequence <- as.character(bepi$Sequence)
+bepi$Tool <- "Bepipred 2.0"
 
 ## CALCULATE SEQUENCE LENGTH
 len <- c()
-for (x in 1:length(bebi$Sequence)){
-  x <- nchar(bebi$Sequence[x])
+for (x in 1:length(bepi$Sequence)){
+  x <- nchar(bepi$Sequence[x])
   len <- c(len, x)
 }
 
 ## ADD LENGTH TO RESULTS
-bebi$Length <- len
-glimpse(bebi)
+bepi$Length <- len
+glimpse(bepi)
 
 ## RENAME SCORE COLUMNS
 abc <- rename(abc, Score = "ABCscore")
-bebi <- rename(bebi, Score = "BebiScore")
+bepi <- rename(bepi, Score = "BebiScore")
 
 ## BIND DATAFRAMES INTO LINEAR RESULTS
-linear <- rbind(bebi, abc)
-glimpse(linear)
+linear <- rbind(bepi, abc)
 
 ## IMPORT STRUCTURAL RESULTS
-disc <- read.csv("path/to/C_epixtractor/discotope_Out.csv", sep = ";")
-
-### ADD MORE RESULTS IF MORE STRUCTURES WERE ANALYZED
-'''
-disc2 <- read.csv("/Users/rocfarriolduran/Desktop/BSC/B_SARS-CoV-2/A_SARS2_DEVELOPMENT/20210920_R1A_brewpitopes/C_epixtractor/7cz4_Out.csv", sep = ";")
-disc3 <- read.csv("/Users/rocfarriolduran/Desktop/BSC/B_SARS-CoV-2/A_SARS2_DEVELOPMENT/20210920_R1A_brewpitopes/C_epixtractor/7d47_Out.csv", sep = ";")
-disc4 <- read.csv("/Users/rocfarriolduran/Desktop/BSC/B_SARS-CoV-2/A_SARS2_DEVELOPMENT/20210920_R1A_brewpitopes/C_epixtractor/7exm_Out.csv", sep = ";")
-#disc5 <- read.csv("/Users/rocfarriolduran/Desktop/BSC/B_SARS-CoV-2/A_SARS2_DEVELOPMENT/20210914_R1AB_brewpitopes/C_epixtractor/7kag_819-929_Out.csv", sep = ";")
-#disc6 <- read.csv("/Users/rocfarriolduran/Desktop/BSC/B_SARS-CoV-2/A_SARS2_DEVELOPMENT/20210914_R1AB_brewpitopes/C_epixtractor/6zlw_1-180_Out.csv", sep = ";")
-#disc7 <- read.csv("/Users/rocfarriolduran/Desktop/BSC/B_SARS-CoV-2/A_SARS2_DEVELOPMENT/20210914_R1AB_brewpitopes/C_epixtractor/6zct_4236-4384_Out.csv", sep = ";")
-#disc8 <- read.csv("/Users/rocfarriolduran/Desktop/BSC/B_SARS-CoV-2/A_SARS2_DEVELOPMENT/20210914_R1AB_brewpitopes/C_epixtractor/6wuu_1562-1879_Out.csv", sep = ";")
-#disc9 <- read.csv("/Users/rocfarriolduran/Desktop/BSC/B_SARS-CoV-2/A_SARS2_DEVELOPMENT/20210914_R1AB_brewpitopes/C_epixtractor/6w4b_4141-4253_Out.csv", sep = ";")
-#disc10 <- read.csv("/Users/rocfarriolduran/Desktop/BSC/B_SARS-CoV-2/A_SARS2_DEVELOPMENT/20210914_R1AB_brewpitopes/C_epixtractor/5s71_6543-6798_Out.csv", sep = ";")
-'''
-
-### MERGE THE DIFFERENT STRUCTURAL RESULTS
-disc_all <- rbind(disc)#, disc2, disc4, disc4)
-colnames(disc_all)
-glimpse(disc_all)
+disc <- read.csv(file = argv$discotope, sep = ";")
 
 ## ADD VARIABLES
-disc_all$Sequence <- as.character(disc_all$Sequence)
-#disc_all$Length = apply(disc,1, function(x) nchar(disc_all$Sequence[x]))
+disc$Sequence <- as.character(disc$Sequence)
 
 ## CALCULATE EPITOPE LENGTH
 len2 <- c()
-for (x in 1:length(disc_all$Sequence)){
-  x <- nchar(disc_all$Sequence[x])
+for (x in 1:length(disc$Sequence)){
+  x <- nchar(disc$Sequence[x])
   len2 <- c(len2, x)
 }
 
-disc_all$Length <- len2
+disc$Length <- len2
 
 ## ADD PREDICTION TOOL
-disc_all$Tool <- "Discotope 2.0"
-glimpse(disc_all)
-glimpse(linear)
+disc$Tool <- "Discotope 2.0"
 
 ## BIND DATAFRAMES
-merged <- rbind(linear, disc_all) #since no discotope predictions
-glimpse(merged)
+merged <- rbind(linear, disc) 
 
 ## FILTER LENGTH 0 FROM ABCPRED (optional)
 merged <- filter(merged, Length != 0)
 
 ## EXPORT DATAFRAME
-write.table(merged, "D_epimerger/XXX_linear_structural.csv", row.names = F, quote = F, sep = ";")
+write.table(merged, file = paste0(argv$outdir, "/", argv$sample, ".csv"), row.names = F, quote = F, sep = ";")
+
+### EXPORT RDATA
+#p <- add_argument(p, "--save_rdata_dir", help = "Path to save rData image", type = "character", default = ".")
+# dir.create(paste0(argv$save_rdata_dir, "/rdata"))
+# save.image(paste0(argv$save_rdata_dir, "/rdata/", argv$sample, ".rdata"))
