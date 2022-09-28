7c8a2f8007e2c332a7d4844c52d38e4851e88f04
rocfd <56597768+rocfd@users.noreply.github.com>
2022-09-28 10:26:55 +0200

    directories automated

--- directories.R
+++ directories.R
@@ -1,24 +1,45 @@
 ## MAKE DIRECTORIES FOR BREWPITOPES
 
+library(argparser)
+
+### SCRIPT ARGUMENTS
+# Create a parser
+p <- arg_parser("PROJECT DIRECTORIES")
+
+# Add command line arguments
+p <- add_argument(p, "--path", help= "Path to project directory", type="character", default = "./brewpitopes")
+
+# Parse the command line arguments
+argv <- parse_args(p)
+
+### CREATE PROJECT DIRECTORY
+dir.create(argv$path)
+
+### BREWPITOPES DIRECTORY
+dir.create(paste0(argv$path, "/", "brewpitopes", sep = ""))
+
 ## INITIAL DATA - FASTA
-dir.create("Z_fasta")
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "Z_fasta", sep = ""))
 
 ## DIRECTORIES
-dir.create("A_linear_predictions")
-dir.create("A_linear_predictions/bebipred")
-dir.create("A_linear_predictions/abcpred")
-dir.create("B_structural_predictions")
-dir.create("B_structural_predictions/discotope")
-dir.create("B_structural_predictions/pdb")
-dir.create("B_structural_predictions/alphafold")
-dir.create("C_epixtractor")
-dir.create("D_epimerger")
-dir.create("E_epitoplogy")
-dir.create("E_epitoplogy/CCTOP")
-dir.create("F_epiglycan")
-dir.create("F_epiglycan/netnglyc")
-dir.create("F_epiglycan/netoglyc")
-dir.create("G_episurf")
-dir.create("H_epifilter")
-dir.create("I_final_candidates")
-dir.create("J_plots")
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "A_linear_predictions", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "A_linear_predictions/bepipred", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "A_linear_predictions/abcpred", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "B_structural_predictions", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "B_structural_predictions/discotope", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "B_structural_predictions/pdb", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "B_structural_predictions/alphafold", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "B_structural_predictions/pdbrenum", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "C_epixtractor", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "D_epimerger", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "E_epitopology", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "E_epitopology/CCTOP", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "F_epiglycan", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "F_epiglycan/netnglyc", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "F_epiglycan/netoglyc", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "G_episurf", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "G_episurf/icm", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "H_epifilter", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "I_final_candidates", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "J_plots", sep = ""))
+dir.create(paste0(argv$path, "/", "brewpitopes", "/", "K_epitope_regions", sep = ""))
