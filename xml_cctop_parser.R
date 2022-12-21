91c92d590a9d1f193cd73aee36165b4797dc6626
rocfd <56597768+rocfd@users.noreply.github.com>
2022-12-21 16:03:47 +0100

    if empty

--- xml_cctop_parser.R
+++ xml_cctop_parser.R
@@ -26,30 +26,35 @@ p <- add_argument(p, "--outdir", help = "Path to output files", type = "characte
 argv <- parse_args(p)
 
 ## LOAD XML
-data <- xmlParse(argv$xml)
+data <- xmlParse("/Users/rocfarriolduran/Desktop/BSC/B_SARS-CoV-2/A_SARS2_DEVELOPMENT/20221212_dockerized_final_test/brewpitopes/Projects/marina/brewpitopes/E_epitopology/CCTOP/cctop.xml")
 
 ## TRANSFORM TO LIST
 xml_data <- xmlToList(data)
 
 ## LIST TO DATAFRAME
 xml_df <- xml_data$Topology
+glimpse(xml_df)
 
 ### EXTRACT ALL THE TOPOLOGY DOMAINS
 ### ADD ONE DATAFRAME PER DOMAIN
 ## EXTRACT ALL REGIONS INTO SEPPARATE DATAFRAMES
 ## SKIP LAST DATAFRAME SINCE IT DOES NOT CONTAIN TOPOLOGY INFORMATION
-for(i in 1:(length(xml_df)-1)) {
-  assign(paste0("topdf.", i), as.data.frame(t(xml_df[[i]])))
+
+### IF EMPTY
+if(is.null(xml_df)){
+  print("STOPPER!! Your target protein has no predicted extracellular domains. Hence, neutralizing antibodies will not recognize it. You should consider another protein from your target organism".)
+} else {
+  for(i in 1:(length(xml_df)-1)) {
+    assign(paste0("topdf.", i), as.data.frame(t(xml_df[[i]])))
+  }
+  
+  ### MERGE DOMAINS INTO A SINGLE DATAFRAME
+  top_df <- mget(ls(pattern="^topdf\\.\\d+")) %>% bind_rows()
+  
+  ## EXPORT DATAFRAME
+  write.csv(top_df, file = paste0(argv$outdir, "/", argv$sample, sep = ""), row.names = F, quote = F)
+
 }
-
-### MERGE DOMAINS INTO A SINGLE DATAFRAME
-top_df <- mget(ls(pattern="^topdf\\.\\d+")) %>% bind_rows()
-
-## EXPORT DATAFRAME
-write.csv(top_df, file = paste0(argv$outdir, "/", argv$sample, sep = ""), row.names = F, quote = F)
-
-## FINAL PRINT
-print(paste("Find your parsed topology domains at: ", argv$outdir, "/", argv$sample, sep = ""))
 
 ### EXPORT RDATA
 #p <- add_argument(p, "--save_rdata_dir", help = "Path to save rData image", type = "character", default = ".")
