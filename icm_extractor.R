42ba17e12edb06fdd2af62e2158b6a06a6d9b427
rocfd <56597768+rocfd@users.noreply.github.com>
2022-12-22 12:42:49 +0100

    update on parse and new file and new chain argument

--- icm_extractor.R
+++ icm_extractor.R
@@ -13,9 +13,10 @@ library(argparser, quietly = T, warn.conflicts = F)
 p <- arg_parser("ICM RSA PARSER")
 
 # Add command line arguments
-p <- add_argument(p, "--icm", help= "Path to icm output of CCTOP (.csv format)", type="character", default = "/brewpitopes/G_episurf/")
+p <- add_argument(p, "--icm", help= "Path to icm output of ICM (.csv format)", type="character", default = "G_episurf/icm/rsa.csv")
 p <- add_argument(p, "--sample", help = "Sample name to label output files (NO extension)", type = "character", default = "buried_positions")
 p <- add_argument(p, "--outdir", help = "Path to output files", type = "character", default = "G_episurf")
+p <- add_argument(p, "--chain", help = "Chain of interest of the target PDB", type = "character", default = "A")
 #p <- add_argument(p, "--save_rdata_dir", help = "Path to save rData image", type = "character", default = "G_episurf")
 
 # Parse the command line arguments
@@ -25,20 +26,37 @@ argv <- parse_args(p)
 icm <- read.csv(file = argv$icm)
 
 ## SPLIT RESIDUE COLUMN
-icm_res <- data.frame(do.call("rbind", strsplit(as.character(icm$residue), ".", fixed = TRUE)))
+#icm_res <- data.frame(do.call("rbind", strsplit(as.character(icm$residue), ".", fixed = TRUE)))
 
 ## BIND RESIDUE COLUMN
-icm <- cbind(icm, icm_res)
+#icm <- cbind(icm, icm_res)
 
-### RENAME AND REFORMAT COLUMNS
-icm <- rename(icm, chain = "X1")
-icm <- rename(icm, aa = "X2")
-icm <- rename(icm, position = "X3")
-icm$position <- as.numeric(as.character(icm$position))
+# ### RENAME AND REFORMAT COLUMNS
+# icm <- rename(icm, chain = "X1")
+# icm <- rename(icm, aa = "X2")
+# icm <- rename(icm, position = "X3")
+# icm$position <- as.numeric(as.character(icm$position))
+
+### NEW PARSING
+
+## MID FUNCTION
+mid = function(text, start_num, num_char) {
+  substr(text, start_num, start_num + num_char - 1)
+}
+
+### PARSE CHAIN
+icm$chain <- mid(icm$residue, 1, 1)
+
+### PARSE RESIDUE
+icm$aa <- mid(icm$residue, 3, 1)
+
+### PARSE POSITION
+icm$position <- as.numeric(gsub("\\D", "", icm$residue))
+#glimpse(icm)
 
 ### REORDER BY CHAIN
 icm <- arrange(icm,chain)
-icm <- filter(icm, chain == "A")
+icm <- filter(icm, chain == argv$chain)
 
 ### DEFINE TO FILTER NETSURF RESULTS
 icm_position <- icm$position
