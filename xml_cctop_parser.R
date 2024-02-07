c4f315dae8b934b52363f0d8ed94a580b3ee80cb
Victor Montal <vmontalb@pop-os.localdomain>
2024-02-07 15:45:40 +0100

    fix(parser): adapt parser to make it match brewpitope scheme

--- xml_cctop_parser.R
+++ xml_cctop_parser.R
@@ -1,62 +1,57 @@
 ## CCTOP RESULTS PARSER
 ## GOAL: extract the extracellular domains from .xml CCTOP results.
 
+# Created by: Roc Farriol
+#    XX XX 2022
+# 
+# Modified by: Victor Montal
+#    7 Feb 2024
+
 ## PREDICT TOPOLOGY USING CCTOP
 ## http://cctop.enzim.ttk.mta.hu/?_=/jobs/submit
 ## DOWNLOAD RESULTS AS .XML
 
-## LIBRARIES
+
+# --
+## Libraries
+# --
+rm(list = ls())
 library(argparser, quietly = T, warn.conflicts = F)
 library(dplyr, quietly = T, warn.conflicts = F)
-
-## XML PARSER
 library(XML, quietly = T, warn.conflicts = F)
 
-### SCRIPT ARGUMENTS
+# --
+## SCRIPT PARSER
+# --
 # Create a parser
 p <- arg_parser("XML CCTOP PARSER")
-
-# Add command line arguments
-p <- add_argument(p, "--xml", help= "Path to XML output of CCTOP (.xml format)", type="character", default = ".")
-p <- add_argument(p, "--sample", help = "Sample name to label output files", type = "character", default = "cctop_domains.csv")
-p <- add_argument(p, "--outdir", help = "Path to output files", type = "character", default = "E_epitopology/CCTOP")
-#p <- add_argument(p, "--save_rdata_dir", help = "Path to save rData image", type = "character", default = "E_epitopology/CCTOP")
-
-# Parse the command line arguments
+p <- add_argument(p, "--path", help= "Path to the brewpitope project", type="character")
 argv <- parse_args(p)
 
-## LOAD XML
-data <- xmlParse(argv$xml)
+# --
+## Define defaults 
+# --
+argv$path <- "/home/vmontalb/Desktop/todel/brewpitopes"
+ixml <- paste0(argv$path,"/E_epitopology/CCTOP/cctop.xml")
+imerged <- paste0(argv$path,"/D_epimerger/merged.csv")
+ofile <- paste0(argv$path,"/E_epitopology/CCTOP/cctop_domains.csv")
 
-## TRANSFORM TO LIST
-xml_data <- xmlToList(data)
+# --
+## Operate CCTOP xml file
+# --
+data <- xmlParse(ixml)        # parse xml
+xml_data <- xmlToList(data)   # transform to table
+xml_df <- xml_data$CCTOPItem$Topology   # transform to df
 
-## LIST TO DATAFRAME
-xml_df <- xml_data$Topology
-#glimpse(xml_df)
-
-### EXTRACT ALL THE TOPOLOGY DOMAINS
-### ADD ONE DATAFRAME PER DOMAIN
-## EXTRACT ALL REGIONS INTO SEPPARATE DATAFRAMES
-## SKIP LAST DATAFRAME SINCE IT DOES NOT CONTAIN TOPOLOGY INFORMATION
-
-### IF EMPTY
+# Loop over all topology domains and merge
 if(is.null(xml_df)){
   print("STOPPER!! Your target protein has no predicted extracellular domains. Hence, neutralizing antibodies will not recognize it. You should consider another protein from your target organism.")
 } else {
   for(i in 1:(length(xml_df)-1)) {
     assign(paste0("topdf.", i), as.data.frame(t(xml_df[[i]])))
   }
-  
-  ### MERGE DOMAINS INTO A SINGLE DATAFRAME
+  # merge
   top_df <- mget(ls(pattern="^topdf\\.\\d+")) %>% bind_rows()
-  
-  ## EXPORT DATAFRAME
-  write.csv(top_df, file = paste0(argv$outdir, "/", argv$sample, sep = ""), row.names = F, quote = F)
-
+  # export
+  write.csv(top_df, file=ofile , row.names=F, quote=F)
 }
-
-### EXPORT RDATA
-#p <- add_argument(p, "--save_rdata_dir", help = "Path to save rData image", type = "character", default = ".")
-#dir.create(paste0(argv$save_rdata_dir, "/rdata", sep = ""))
-#save.image(paste0(argv$save_rdata_dir, "/rdata/", argv$sample, ".rdata", sep = ""))
