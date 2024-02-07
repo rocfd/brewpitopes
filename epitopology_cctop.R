470726ad4a2219f58c74c1e0995bb8bf1cc3c37b
Victor Montal <vmontalb@pop-os.localdomain>
2024-02-07 16:02:15 +0100

    fix(parser): modify to make it work for brewpitope new data structure

--- epitopology_cctop.R
+++ epitopology_cctop.R
@@ -1,48 +1,55 @@
 ## EPITOPOLOGY
 ## GOAL: label epitopes on the viral topologyace based on topology information.
 
+# Created by: Roc Farriol
+#    XX XX 2022
+# 
+# Modified by: Victor Montal
+#    7 Feb 2024
+
+# --
+## Libraries
+# --
+rm(list = ls())
 library(dplyr, quietly = T, warn.conflicts = F)
 library(tidyr, quietly = T, warn.conflicts = F)
 library(stringr, quietly = T, warn.conflicts = F)
 library(argparser, quietly = T, warn.conflicts = F)
 
-### SCRIPT ARGUMENTS
-# Create a parser
+
+# --
+## SCRIPT PARSER
+# --
 p <- arg_parser("EPITOPOLOGY from CCTOP")
-
-# Add command line arguments
-p <- add_argument(p, "--input_CCTOP", help= "Path to CCTOP output dataset from XML parser (.csv format)", type="character", default = "E_epitopology/CCTOP/cctop_domains.csv")
-p <- add_argument(p, "--input_epitopes", help= "Path to epitope dataset resulting from Epimerger.py (.csv format)", type="character", default = "D_epimerger/merged.csv")
-p <- add_argument(p, "--sample", help = "Sample name to label output files", type = "character", default = "topology_extracted.csv")
-p <- add_argument(p, "--outdir", help = "Path to output files", type = "character", default = "E_epitopology")
-# p <- add_argument(p, "--save_rdata_dir", help = "Path to save rData image", type = "character", default = "E_epitoplogy")
-
-# Parse the command line arguments
+p <- add_argument(p, "--path", help= "Path to the brewpitope project", type="character")
 argv <- parse_args(p)
 
-## UPLOAD TOPOLOGY DATA
-## OPTION 1
-## IMPORT DATAFRAME from CCTOP PREDICTIONS
-topology <- read.csv(file = argv$input_CCTOP, header = T)
-
-### IMPORT DATAFRAME from EPIMERGER.PY
-merged <- read.csv(file = argv$input_epitopes, header = T, sep = ";")
-
-### FILTER EXTRACELLULAR DOMAINS
-topology <- filter(topology, loc == "O" | loc == "S")
-
-### RENAME COLUMNS
+# --
+## Define defaults 
+# --
+icctop <- paste0(argv$path,"/E_epitopology/CCTOP/cctop_domains.csv")
+imerged <- paste0(argv$path,"/D_epimerger/merged.csv")
+ofile <- paste0(argv$path,"/E_epitopology/CCTOP/topology_extracted.csv")
+  
+# --
+## Load topology and merged df
+# --
+topology <- read.csv(file=icctop, header = T)
+topology <- filter(topology, loc == "O" | loc == "S")      # filter segment 
 topology <- rename(topology, extracellular.start = "from")
 topology <- rename(topology, extracellular.end = "to")
-
-## CHARACTER TO NUMBER
 topology$extracellular.start <- as.numeric(topology$extracellular.start)
 topology$extracellular.end <- as.numeric(topology$extracellular.end)
 
-### LOOP TO LABEL EXTRACELLULARITY
+merged <- read.csv(file=imerged, header = T, sep = ";")
+
+# --
+## Label Epitopes based on extracellularity
+# --
 extracellular_list <- list()
 extracellular_epitope <- c()
 extracellular_label <- c()
+
 for (x in 1:length(merged$Sequence)){
   for (z in 1:length(topology$extracellular.start)){
     y <- ifelse(merged$Start[x] >= topology$extracellular.start[z] & merged$End[x] <= topology$extracellular.end[z], "Extracellular", "Non-extracel")
@@ -54,17 +61,12 @@ for (x in 1:length(merged$Sequence)){
   extracellular_label <- c()
 }
 
-### ADD LIST TO DATAFRAME
 merged$Extracellular <- extracellular_list
-
-### TRANSFORM LIST TO CHARACTER
 merged$Extracellular <- as.character(merged$Extracellular)
 
-## EXPORT RESULTS
-write.table(merged, sep = ";", file = paste0(argv$outdir, "/", argv$sample, sep = ""), quote = F, row.names = F)
-print(paste("Find your output file at: ", argv$outdir, "/", argv$sample, sep = ""))
 
-### EXPORT RDATA
-# #p <- add_argument(p, "--save_rdata_dir", help = "Path to save rData image", type = "character", default = ".")
-# dir.create(paste0(argv$save_rdata_dir, "rdata", sep = ""))
-# save.image(paste0(argv$save_rdata_dir, "rdata/", argv$sample, ".rdata", sep = ""))
+# --
+## Export results
+# --
+write.table(merged, sep=";", file=ofile, quote = F, row.names = F)
+print(paste("Find your output file at: ", ofile))
